31cdaaf147d848ba74cf3bee5e557738f1840a20
arjun.esguerra <arjun.esguerra@hmcarchitects.com>
2024-07-31 13:17:10 -0700

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HMC_Boarding_v2;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
